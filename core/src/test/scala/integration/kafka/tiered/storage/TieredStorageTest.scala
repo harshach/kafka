c2723c2003d01296bde6ac151d16180acc7b63e1
Alexandre Dupriez <duprie@amazon.com>
2020-04-08 18:38:11 +0100

    Use the index size instead of log segment size to enable the creation of a single-record log segment.

--- core/src/test/scala/integration/kafka/tiered/storage/TieredStorageTest.scala
+++ core/src/test/scala/integration/kafka/tiered/storage/TieredStorageTest.scala
@@ -15,6 +15,7 @@ import org.apache.kafka.common.config.TopicConfig
 import org.apache.kafka.common.log.remote.storage.LocalTieredStorage.{DELETE_ON_CLOSE_PROP, STORAGE_ID_PROP}
 import org.apache.kafka.common.log.remote.storage.LocalTieredStorageWaiter.newWaiter
 import org.apache.kafka.common.log.remote.storage.{LocalTieredStorage, LocalTieredStorageSnapshot, RLMMWithTopicStorage}
+import org.apache.kafka.common.record.{AbstractRecords, CompressionType, DefaultRecord, DefaultRecordBatch, RecordBatch}
 import org.apache.kafka.common.serialization.StringSerializer
 import org.junit.Assert.assertEquals
 import org.junit.{Assert, Test}
@@ -29,7 +30,7 @@ class TieredStorageTest extends IntegrationTestHarness {
     overridingProps.setProperty(KafkaConfig.RemoteLogManagerTaskIntervalMsProp, 1000.toString)
     overridingProps.setProperty(KafkaConfig.RemoteLogMetadataTopicReplicationFactorProp, 1.toString)
 
-    overridingProps.setProperty(STORAGE_ID_PROP, "phoque")
+    overridingProps.setProperty(STORAGE_ID_PROP, "tiered-storage")
     overridingProps.setProperty(DELETE_ON_CLOSE_PROP, "true")
 
     createBrokerConfigs(numConfigs = 1, zkConnect).map(KafkaConfig.fromProps(_, overridingProps))
@@ -39,8 +40,15 @@ class TieredStorageTest extends IntegrationTestHarness {
 
   @Test
   def test(): Unit = {
+    val logDirectory = configs(0).get(KafkaConfig.LogDirProp)
+
+    //
+    // Leverage the use of the segment index size to create a log-segment accepting one and only one record.
+    // The minimum size of the indexes is that of an entry, which is 8 for the offset index and 12 for the
+    // time index. Hence, with 12, we can guarantee no more than one entry in both the offset and time index.
+    //
     val topicProps = new Properties()
-    topicProps.setProperty(TopicConfig.SEGMENT_BYTES_CONFIG, 69.toString)
+    topicProps.setProperty(TopicConfig.SEGMENT_INDEX_BYTES_CONFIG, 12.toString)
 
     TestUtils.createTopic(zkClient, "phoque", brokerCount, brokerCount, servers, topicProps)
 
@@ -61,6 +69,8 @@ class TieredStorageTest extends IntegrationTestHarness {
 
     val records = snapshot.getRecords
     assertEquals(1, records.size())
+
     Assert.assertEquals(util.Arrays.asList(ByteBuffer.wrap("a".getBytes)), records.values().iterator().next())
   }
+
 }
