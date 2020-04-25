4f4eb2a25fa46592ec30aa81de802eb264bfddf6
Alexandre Dupriez <duprie@amazon.com>
2020-04-25 20:42:43 +0100

    Add javadoc for LocalTieredStorageCondition.

--- clients/src/test/java/org/apache/kafka/common/log/remote/storage/LocalTieredStorageCondition.java
+++ clients/src/test/java/org/apache/kafka/common/log/remote/storage/LocalTieredStorageCondition.java
@@ -26,6 +26,22 @@ import java.util.concurrent.TimeoutException;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
+/**
+ * A {@link LocalTieredStorageCondition} embeds a subset of the characterization of an interaction
+ * with the {@link LocalTieredStorage} and provides a method which suspends the current thread until
+ * an interaction with the storage which matches the characterization is intercepted.
+ *
+ * In this implementation, the elements characterizing an interaction between the {@link LocalTieredStorage}
+ * and a broker include the nature of that interaction (e.g. offload or fetch a segment, fetch a time index, etc.),
+ * the broker from which the interaction originates from, the topic-partition the interaction relates
+ * to, and whether the interaction results in a successful or failed outcome.
+ *
+ * The current model for interactions with the {@link LocalTieredStorage} is materialized by
+ * {@link LocalTieredStorageEvent}.
+ *
+ * Conditions can be chained to formulate logical conjunctions via the use of the method
+ * {@link LocalTieredStorageCondition#and(LocalTieredStorageCondition)}.
+ */
 public final class LocalTieredStorageCondition {
     final EventType eventType;
     final int brokerId;
@@ -35,6 +51,22 @@ public final class LocalTieredStorageCondition {
     private final InternalListener listener;
     private final LocalTieredStorageCondition next;
 
+    /**
+     * Constructs a new condition on an interaction of the {@code eventType} nature, from the broker
+     * with id {@code brokerId}, for the topic-partition {@code tp} and which is {@code failed} or not.
+     *
+     * This condition internally observes the provided {@code storages} to intercept such an
+     * interaction if it occurs.
+     *
+     * NOTE: interactions with the {@code storages} is monitored as soon as this method returns.
+     *
+     * @param storages
+     * @param eventType
+     * @param brokerId
+     * @param tp
+     * @param failed
+     * @return
+     */
     public static LocalTieredStorageCondition expectEvent(final Iterable<LocalTieredStorage> storages,
                                                           final EventType eventType,
                                                           final int brokerId,
@@ -46,18 +78,50 @@ public final class LocalTieredStorageCondition {
         return condition;
     }
 
-    public final LocalTieredStorageCondition and(final LocalTieredStorageCondition last) {
-        if (last.next != null) {
+    /**
+     * Formulates a new condition which is true if and only if this condition and the {@code conjuct}
+     * provided as argument are both true.
+     *
+     * @param conjuct Another condition which truth is required for the resulting new condition to be true.
+     * @return A new condition which is true iff this condition and {@code conjuct} are both true.
+     */
+    public final LocalTieredStorageCondition and(final LocalTieredStorageCondition conjuct) {
+        //
+        // To keep things simple, only authorize append to the condition chain of elementary (not composed)
+        // conditions. It also allows to protect from cycles.
+        //
+        if (conjuct.next != null) {
             throw new IllegalArgumentException(
-                    format("The condition %s is already composed, cannot add it to %s", last, this));
+                    format("The condition %s is already composed, cannot add it to %s", conjuct, this));
         }
-        if (last == this) {
+        if (conjuct == this) {
             throw new IllegalArgumentException(
                     format("The condition %s cannot be added to itself", this));
         }
-        return new LocalTieredStorageCondition(this, next != null ? next.and(last) : last);
+        //
+        // Note: There is no upper bound enforced on the length of the chain of conditions.
+        //       This class is for tests only!
+        //
+        return new LocalTieredStorageCondition(this, next != null ? next.and(conjuct) : conjuct);
     }
 
+    /**
+     * Suspend the current thread until the condition(s) materialized by this instance becomes true.
+     * The method returns non-exceptionally only if the truth of the condition is established before
+     * the given {@code timeout} elapses.
+     *
+     * Whether or not this condition has been constructed from multiple conjuncts does not impact the
+     * time limit defined by {@code timeout}, which is always honored regardless of how many underlying
+     * conditions this condition is composed of.
+     *
+     * If the truth of this condition could not be established before timeout, a {@link TimeoutException}
+     * is thrown.
+     *
+     * @param timeout The maximum time the calling thread can be suspended until the condition is true.
+     * @param unit The unit of the timeout.
+     * @throws InterruptedException If this method is interrupted while awaiting the condition to be true.
+     * @throws TimeoutException If the truth of the condition could not be verified before {@code timeout}.
+     */
     public void waitUntilTrue(final long timeout, final TimeUnit unit) throws InterruptedException, TimeoutException {
         final long start = System.currentTimeMillis();
 
